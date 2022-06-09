ae3613ddac8af34a9bf33fad85576a9e110ff4eb
Sam <BESTVULCANCLIENT@gmail.com>
2022-06-09 21:03:35 +1000

    update thing
    sadasdas

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-if (window.open) {
+function move {
+window.posX = math.random();
+  window.posZ = math.random();
+  window.posY = math.random();
   window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ","_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+}
+if (window.open) {
+  move()
+  move()
+  move()
 }
